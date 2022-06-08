de98a8cbf463193d5dffbca84f56d4fdc022505c
Ärtem <horoshevskii@gmail.com>
2022-06-08 16:44:24 +0300

    +

--- src/main/Person.java
+++ src/main/Person.java
@@ -1,3 +1,4 @@
+
 public class Person {
     public static boolean isTeenager(int age) {
         boolean result;
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,7 +1,6 @@
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
